b3aa92aa19ca5fe29d9adeb8997a629fc2fab4d2
miguelbcr <miguel@irewardhealth.com>
2023-05-30 13:45:49 +0200

    Remove buildSrc directory

--- buildSrc/src/main/java/com/shadowflight/buildsrc/OpenApiCodeGenUtils.kt
+++
@@ -1,6 +0,0 @@
-package com.shadowflight.buildsrc
-
-object OpenApiCodeGenUtils {
-    const val PATH_APIS = "core/openapi/src/main/kotlin/com/shadowflight/openapi/apis"
-    const val PATH_DATA = "core/openapi/src/main/kotlin/com/shadowflight/openapi/data"
-}
