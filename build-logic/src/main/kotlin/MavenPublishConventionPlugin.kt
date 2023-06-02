3eb6a9dac9fbe9317c35e4a59483fd4aa2fd7a33
Chema Rubio <chema@significo.com>
2023-06-02 11:41:39 +0200

    feat: generate new convention plugin definition for maven publishing (currently local)

---
+++ build-logic/src/main/kotlin/MavenPublishConventionPlugin.kt
@@ -0,0 +1,27 @@
+import com.android.build.api.dsl.LibraryExtension
+import com.shadowflight.sdk.configureDocsAndSources
+import com.shadowflight.sdk.configureMavenPublish
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.kotlin.dsl.configure
+
+@Suppress("unused")
+class MavenPublishConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+
+            pluginManager.apply{
+                apply("maven-publish")
+            }
+
+            extensions.configure<LibraryExtension> {
+                configureDocsAndSources(this)
+            }
+
+            extensions.configure<PublishingExtension> {
+                configureMavenPublish(this)
+            }
+        }
+    }
+}
