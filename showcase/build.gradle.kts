2cb3386d007b9980c520d688b47fd6120aa505ae
Chema Rubio <chema@significo.com>
2023-07-21 11:32:48 +0200

    chore(build): overall dependencies check and update

--- showcase/build.gradle.kts
+++ showcase/build.gradle.kts
@@ -59,7 +59,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.7"
+        kotlinCompilerExtensionVersion = "1.4.8"
     }
 }
 
@@ -70,13 +70,13 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.47")
     implementation("androidx.navigation:navigation-compose:2.6.0")
 
-    kapt("com.google.dagger:hilt-android-compiler:2.46.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.47")
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
 
-    implementation(platform("androidx.compose:compose-bom:2023.04.01"))
+    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
