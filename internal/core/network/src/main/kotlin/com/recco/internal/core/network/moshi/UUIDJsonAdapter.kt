e9955d9794e3113d51eda6a8886913fcc05959d9
saulmm <saulmm2@gmail.com>
2023-11-16 13:15:36 +0100

    Added an UUIDJsonAdapter.kt

---
+++ internal/core/network/src/main/kotlin/com/recco/internal/core/network/moshi/UUIDJsonAdapter.kt
@@ -0,0 +1,18 @@
+package com.recco.internal.core.network.moshi
+
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.ToJson
+import java.util.UUID
+
+class UUIDJsonAdapter {
+
+    @FromJson
+    fun fromJson(value: String): UUID {
+        return UUID.fromString(value)
+    }
+
+    @ToJson
+    fun toJson(value:java.util.UUID): String {
+        return value.toString()
+    }
+}
