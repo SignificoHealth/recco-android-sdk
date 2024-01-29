0cb34bf86b68a13b70b35e3ad49b1364f5bfcfd1
Saul <saul@significo.com>
2024-01-29 10:46:04 +0100

    Added some missing padding

--- internal/feature/media/src/main/java/com/recco/internal/feature/media/player/MediaPlayerScreen.kt
+++ internal/feature/media/src/main/java/com/recco/internal/feature/media/player/MediaPlayerScreen.kt
@@ -32,6 +32,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -288,6 +289,8 @@ private fun AudioHeader(audio: Audio) {
         Text(
             text = audio.headline,
             style = AppTheme.typography.h1.copy(color = Color.White),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(horizontal = AppSpacing.dp_16)
         )
 
         val audioDurationLabel = remember {
