021e613a126cefcf886383f7ac3130523d27fdbb
saulmm <saul@significo.com>
2024-02-01 16:46:17 +0100

    Simplified null check

--- internal/core/media/src/main/java/com/recco/internal/core/media/mapper/TrackItemMapper.kt
+++ internal/core/media/src/main/java/com/recco/internal/core/media/mapper/TrackItemMapper.kt
@@ -4,19 +4,17 @@ import com.recco.internal.core.model.media.MediaType
 import com.recco.internal.core.model.recommendation.Article
 import com.recco.internal.core.model.recommendation.TrackItem
 
-fun Article.asTrackItem(): TrackItem {
-    require(audioUrl != null) {
-        "Cannot map an Article to TrackItem with a null audioUrl"
+fun Article.asTrackItemOrNull(): TrackItem? {
+    return audioUrl?.let {
+        TrackItem(
+            id = id.itemId,
+            mediaUrl = it,
+            title = headline,
+            imageUrl = imageUrl,
+            mediaType = MediaType.AUDIO,
+            lengthInMs = readingTimeInSeconds?.let { seconds ->
+                (seconds * 1000).toLong()
+            }
+        )
     }
-
-    return TrackItem(
-        id = id.itemId,
-        mediaUrl = checkNotNull(audioUrl),
-        title = headline,
-        imageUrl = imageUrl,
-        mediaType = MediaType.AUDIO,
-        lengthInMs = readingTimeInSeconds?.let {
-            (it * 1000).toLong()
-        }
-    )
 }
